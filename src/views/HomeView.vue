d482b824bba00f3d5e7ab5719c190abb8f2332a6
soybean15 <marlonpadilla1593@gmail.com>
2023-12-24 23:47:53 +0800

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
   
   <Drawer/>
   <div class=" flex justify-center   mt-20 ">
-    <div class="mx-5 md:mx-20 w-full  flex bg-black aspect-auto p-1 md:p-3  ">
+    <div class="mx-1 md:mx-20 w-full  flex bg-black aspect-auto p-1 md:p-3  ">
       <hero/>
     </div>
    
